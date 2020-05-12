5a133506185c920d8eafd58259c7b34629c368bc
Dominic <dominic.pr93@gmail.com>
2020-05-12 15:44:31 +0200

    added purge paths to remove unused tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
   theme: {},
   variants: {},
   plugins: []
