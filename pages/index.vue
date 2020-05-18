eb109b909963cf3346c63c87e6c9673c02154da4
Dominic <dominic.pr93@gmail.com>
2020-05-18 23:35:08 +0200

    basic html

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="container">
-    <div class="text-blue-600 hover:text-red-300 cursor-move">hey</div>
+  <div>
+    <section>
+      <h1>Intro</h1>
+      <p>A short explanation what this site is about</p>
+    </section>
+    <section>
+      <h2>Bio</h2>
+      <p>A few short sentences about me to make me seem more human, but no clichee sentences.</p>
+    </section>
+    <aside>
+      <h2>Page Settings</h2>
+      <ul>
+        <li>Dark Mode</li>
+        <li>Switch Background / Style</li>
+        <li>...</li>
+      </ul>
+    </aside>
+    <section>
+      <h2>List of previous Projects with Images and a short description</h2>
+      <ul>
+        <li>A Slider? How to make it not boring?</li>
+        <li>Left Right Layout? Seems to boring as well</li>
+        <li>A cube you can rotate? Maybe...</li>
+      </ul>
+    </section>
   </div>
 </template>
 
@@ -12,10 +35,13 @@ export default Vue.extend({
 });
 </script>
 
-<style>
+<style lang="scss">
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
+section {
+  @apply h-screen w-full bg-blue-300;
+}
 </style>
